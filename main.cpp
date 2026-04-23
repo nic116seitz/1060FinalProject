84ffbc01ebc717700c68e37210a1c1727df20d7a
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2026-04-23 07:24:20 -0700

    Removed unnecessary line breaks

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <ostream>
 #import "CollegeEmployee.h"
 using namespace std;
+void menu() {
+  cout << "From the following menu please enter the number corresponding to your selection: " << endl;
+  cout << "1. Add new faculty" << endl;
+  cout << "2. Add new student" << endl;
+  cout << "3. List all current profiles" << endl;
+  cout << "4. Edit existing profile" << endl;
+}
 
 int main () {
   CollegeEmployee employee;
@@ -16,44 +23,44 @@ int main () {
   
   cout << "Enter first name: ";
   cin >> fName;
-  cout << endl; 
+   
   employee.SetFirst(fName);
 
   cout << "Enter last name: ";
   cin >> lName;
-  cout << endl;
+  
   employee.SetLast(lName);
 
   cout << "Enter age: ";
   cin >> age;
-  cout << endl;
+  
   employee.SetAge(age);
 
   cout << "Enter address: ";
   cin.ignore();
   getline(cin, address);
-  cout << endl;
+  
   employee.SetAddress(address);
 
   cout << "Enter Social Security Number (SSN): ";
   cin >> ssn;
-  cout << endl;
+  
   employee.SetSSN(ssn);
  
   cout << "Enter Annual Comp: ";
   cin >> anComp;
-  cout << endl;
+  
   employee.SetAnnualComp(anComp);
 
   cout << "Enter Department Name: ";
   cin.ignore();
   getline(cin, deptName);
-  cout << endl;
+  
   employee.SetDeptName(deptName);
 
   cout << "Enter Job: ";
   cin >> job;
-  cout << endl;
+  
   employee.SetJob(job);
   
   employee.GetInfo();
